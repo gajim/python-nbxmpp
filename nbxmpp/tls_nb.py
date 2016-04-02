02343d2162d56fb06f0c8831414dae747f739e3a
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2016-04-02 16:48:48 +0100

    Fix deprecation warning, cipher_list must be bytes and not str.

--- nbxmpp/tls_nb.py
+++ nbxmpp/tls_nb.py
@@ -267,9 +267,9 @@ class NonBlockingTLS(PlugIn):
         self.cacerts = cacerts
         self.mycerts = mycerts
         if cipher_list is None:
-            self.cipher_list = 'HIGH:!aNULL:RC4-SHA'
+            self.cipher_list = b'HIGH:!aNULL:RC4-SHA'
         else:
-            self.cipher_list = cipher_list
+            self.cipher_list = cipher_list.encode('ascii')
         if tls_version is None:
             self.tls_version = '1.0'
         else:
