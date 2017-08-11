7fedb6c990e6d6e5dfadf4cee7d616e97396a2c4
Yann Leboulanger <yann@leboulanger.org>
2017-08-11 23:23:46 +0200

    [test] fix comparing xml

--- test/lib/__init__.py
+++ test/lib/__init__.py
@@ -9,5 +9,12 @@ root = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../..')
 sys.path.insert(1, root)
 sys.path.insert(1, root + '/test/lib')
 
+def sortxml(data):
+    sorted(data.attrs.keys())
+    if data.kids:
+        for a in data.kids:
+            if not isinstance(a, str):
+                sortxml(a)
+
 def setup_env():
     pass
