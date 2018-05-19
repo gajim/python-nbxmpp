7256d13ebcde065824da4715df0db399fe9aaf57
Philipp Hörist <forenjunkie@chello.at>
2018-05-19 10:42:30 +0200

    Prepare 0.6.6

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='nbxmpp',
-      version='0.6.5',
+      version='0.6.6',
       description='Non blocking Jabber/XMPP module',
       author='Yann Leboulanger',
       author_email='asterix@lagaule.org',
       url='http://dev.gajim.org/gajim/python-nbxmpp',
       packages=['nbxmpp'],
-)
+      )
