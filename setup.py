9da76c1bfd76c8f85c216c488b36f994b280be05
Philipp Hörist <philipp@hoerist.com>
2018-12-21 13:21:51 +0100

    Move code into setup.cfg

--- setup.py
+++ setup.py
@@ -2,11 +2,4 @@
 
 from setuptools import setup
 
-setup(name='nbxmpp',
-      version='0.9.90.2',
-      description='Non blocking Jabber/XMPP module',
-      author='Yann Leboulanger',
-      author_email='asterix@lagaule.org',
-      url='http://dev.gajim.org/gajim/python-nbxmpp',
-      packages=['nbxmpp', 'nbxmpp.modules'],
-      )
+setup()
