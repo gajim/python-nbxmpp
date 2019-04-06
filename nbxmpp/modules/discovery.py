cf7f06256a30afdcab426fdbc7217627f46f8812
Philipp Hörist <philipp@hoerist.com>
2019-04-06 10:10:29 +0200

    Add Discovery (XEP-0030) module

---
+++ nbxmpp/modules/discovery.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2019 Philipp Hörist <philipp AT hoerist.com>
+#
+# This file is part of nbxmpp.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from nbxmpp.protocol import Iq
+
+
+log = logging.getLogger('nbxmpp.m.discovery')
+
+
+def get_disco_request(namespace, jid, node=None):
+    iq = Iq('get', to=jid, queryNS=namespace)
+    if node is not None:
+        iq.getQuery().setAttr('node', node)
+    return iq
