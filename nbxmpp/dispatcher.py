6dde8442fd1a2742f0966ba705e7832466a6d69e
lovetox <philipp@hoerist.com>
2020-10-26 10:59:02 +0100

    Add Ping module

--- nbxmpp/dispatcher.py
+++ nbxmpp/dispatcher.py
@@ -79,6 +79,7 @@ from nbxmpp.modules.http_upload import HTTPUpload
 from nbxmpp.modules.mam import MAM
 from nbxmpp.modules.vcard_temp import VCardTemp
 from nbxmpp.modules.vcard4 import VCard4
+from nbxmpp.modules.ping import Ping
 from nbxmpp.modules.misc import unwrap_carbon
 from nbxmpp.modules.misc import unwrap_mam
 from nbxmpp.structs import StanzaTimeoutError
@@ -188,6 +189,7 @@ class StanzaDispatcher(Observable):
         self._modules['MAM'] = MAM(self._client)
         self._modules['VCardTemp'] = VCardTemp(self._client)
         self._modules['VCard4'] = VCard4(self._client)
+        self._modules['Ping'] = Ping(self._client)
 
         for instance in self._modules.values():
             for handler in instance.handlers:
---
+++ nbxmpp/modules/ping.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 Philipp Hörist <philipp AT hoerist.com>
+#
+# This file is part of nbxmpp.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+from nbxmpp.protocol import Iq
+from nbxmpp.namespaces import Namespace
+from nbxmpp.task import iq_request_task
+from nbxmpp.modules.base import BaseModule
+from nbxmpp.modules.util import process_response
+
+
+class Ping(BaseModule):
+    def __init__(self, client):
+        BaseModule.__init__(self, client)
+
+        self._client = client
+        self.handlers = []
+
+    @iq_request_task
+    def ping(self, jid):
+        _task = yield
+
+        self._log.info('Ping')
+        response = yield _make_ping_request(jid)
+        yield process_response(response)
+
+
+def _make_ping_request(jid):
+    iq = Iq('get', to=jid)
+    iq.addChild(name='ping', namespace=Namespace.PING)
+    return iq
