5ea69f6715250a689e41a89f3dbe429513b74ae8
Philipp Hörist <philipp@hoerist.com>
2024-04-17 21:57:39 +0200

    new: Add a RFC4647 language filtering implementation
    Thanks to Jonas Schäfer for donating the code
    
    Taken from https://codeberg.org/jssfr/aioxmpp/src/branch/devel/aioxmpp/structs.py

---
+++ nbxmpp/language.py
@@ -0,0 +1,284 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Iterable
+from typing import Iterator
+from typing import Sequence
+from typing import cast
+
+import functools
+
+
+# Copied from https://codeberg.org/jssfr/aioxmpp/src/branch/devel/aioxmpp/structs.py
+
+
+@functools.total_ordering
+class LanguageTag:
+    """
+    Implementation of a language tag. This may be a fully RFC5646 compliant
+    implementation some day, but for now it is only very simplistic stub.
+
+    There is no input validation of any kind.
+    """
+
+    __slots__ = ("_tag",)
+
+    def __init__(self, *, tag: str):
+        if not tag:
+            raise ValueError("tag cannot be empty")
+
+        self._tag = tag
+
+    @property
+    def match_str(self) -> str:
+        """
+        The string which is used for matching two language tags. This is the
+        lower-cased version of `print_str`.
+        """
+        return self._tag.lower()
+
+    @property
+    def print_str(self) -> str:
+        """
+        The stringified language tag.
+        """
+        return self._tag
+
+    @classmethod
+    def fromstr(cls, s: str) -> LanguageTag:
+        """
+        Create a language tag from the given string `s`.
+        """
+        return cls(tag=s)
+
+    def __str__(self) -> str:
+        return self.print_str
+
+    def __eq__(self, other: Any) -> bool:
+        try:
+            return self.match_str == other.match_str
+        except AttributeError:
+            return False
+
+    def __lt__(self, other: Any) -> bool:
+        try:
+            return self.match_str < other.match_str
+        except AttributeError:
+            return NotImplemented
+
+    def __le__(self, other: Any) -> bool:
+        try:
+            return self.match_str <= other.match_str
+        except AttributeError:
+            return NotImplemented
+
+    def __hash__(self) -> int:
+        return hash(self.match_str)
+
+    def __repr__(self) -> str:
+        return "<{}.{}.fromstr({!r})>".format(
+            type(self).__module__, type(self).__qualname__, str(self)
+        )
+
+
+class LanguageRange:
+    """
+    Implementation of a language range. This may be a fully RFC4647 compliant
+    implementation some day, but for now it is only very simplistic stub.
+
+    There is no input validation of any kind.
+
+    LanguageRange instances compare and hash case-insensitively.
+    """
+
+    __slots__ = ("_tag",)
+
+    WILDCARD: LanguageRange
+
+    def __init__(self, *, tag: str):
+        if not tag:
+            raise ValueError("range cannot be empty")
+
+        self._tag = tag
+
+    @property
+    def match_str(self) -> str:
+        """
+        The string which is used for matching two language tags. This is the
+        lower-cased version of the `print_str`.
+        """
+        return self._tag.lower()
+
+    @property
+    def print_str(self) -> str:
+        """
+        The stringified language tag.
+        """
+        return self._tag
+
+    @classmethod
+    def fromstr(cls, s: str) -> LanguageRange:
+        """
+        Create a language tag from the given string `s`.
+        """
+        if s == "*":
+            return cls.WILDCARD
+        return cls(tag=s)
+
+    def __str__(self) -> str:
+        return self.print_str
+
+    def __eq__(self, other: Any) -> bool:
+        try:
+            return self.match_str == other.match_str
+        except AttributeError:
+            return False
+
+    def __hash__(self) -> int:
+        return hash(self.match_str)
+
+    def __repr__(self) -> str:
+        return "<{}.{}.fromstr({!r})>".format(
+            type(self).__module__, type(self).__qualname__, str(self)
+        )
+
+    def strip_rightmost(self) -> LanguageRange:
+        """
+        Strip the rightmost part of the language range. If the new rightmost
+        part is a singleton or ``x`` (i.e. starts an extension or private use
+        part), it is also stripped.
+
+        Return the newly created `LanguageRange`.
+        """
+
+        parts = self.print_str.split("-")
+        parts.pop()
+        if parts and len(parts[-1]) == 1:
+            parts.pop()
+        return type(self).fromstr("-".join(parts))
+
+
+LanguageRange.WILDCARD = LanguageRange(tag="*")
+
+
+def basic_filter_languages(
+    languages: Sequence[LanguageTag], ranges: Iterable[LanguageRange]
+) -> Iterator[LanguageTag]:
+    """
+    Filter languages using the string-based basic filter algorithm described in
+    RFC4647.
+
+    `languages` must be a sequence of `LanguageTag` instances which are
+    to be filtered.
+
+    `ranges` must be an iterable which represent the basic language ranges to
+    filter with, in priority order. The language ranges must be given as
+    `LanguageRange` objects.
+
+    Return an iterator of languages which matched any of the `ranges`. The
+    sequence produced by the iterator is in match order and duplicate-free. The
+    first range to match a language yields the language into the iterator, no
+    other range can yield that language afterwards.
+    """
+
+    if LanguageRange.WILDCARD in ranges:
+        yield from languages
+        return
+
+    found: set[LanguageTag] = set()
+
+    for language_range in ranges:
+        range_str = language_range.match_str
+        for language in languages:
+            if language in found:
+                continue
+
+            match_str = language.match_str
+            if match_str == range_str:
+                yield language
+                found.add(language)
+                continue
+
+            if len(range_str) < len(match_str):
+                if (
+                    match_str[: len(range_str)] == range_str
+                    and match_str[len(range_str)] == "-"
+                ):
+                    yield language
+                    found.add(language)
+                    continue
+
+
+def lookup_language(
+    languages: Sequence[LanguageTag], ranges: Iterable[LanguageRange]
+) -> LanguageTag | None:
+    """
+    Look up a single language in the sequence `languages` using the lookup
+    mechanism described in RFC4647. If no match is found, `None` is
+    returned. Otherwise, the first matching language is returned.
+
+    `languages` must be a sequence of `LanguageTag` objects, while
+    `ranges` must be an iterable of `LanguageRange` objects.
+    """
+
+    for language_range in ranges:
+        while True:
+            try:
+                return next(iter(basic_filter_languages(languages, [language_range])))
+            except StopIteration:
+                pass
+
+            try:
+                language_range = language_range.strip_rightmost()
+            except ValueError:
+                break
+
+    return None
+
+
+class LanguageMap(dict[LanguageTag | None, Any]):
+    """
+    A `dict` subclass specialized for holding `LanugageTag`
+    instances as keys.
+
+    In addition to the interface provided by `dict`, instances of this
+    class also have the following methods:
+    """
+
+    def lookup(self, language_ranges: Sequence[LanguageRange]) -> Any:
+        """
+        Perform an RFC4647 language range lookup on the keys in the
+        dictionary. `language_ranges` must be a sequence of
+        `LanguageRange` instances.
+
+        Return the entry in the dictionary with a key as produced by
+        `lookup_language`. If `lookup_language` does not find a match and the
+        mapping contains an entry with key `None`, that entry is
+        returned, otherwise `KeyError` is raised.
+        """
+        keys = list(self.keys())
+        try:
+            keys.remove(None)
+        except ValueError:
+            pass
+
+        keys = cast(list[LanguageTag], keys)
+        keys.sort()
+        key = lookup_language(keys, language_ranges)
+        return self[key]
+
+    def any(self) -> Any:
+        """
+        Returns any element from the language map, preferring the `None`
+        key if it is available.
+
+        Guarantees to always return the same element for a map with the same
+        keys, even if the keys are iterated over in a different order.
+        """
+        if not self:
+            raise ValueError("any() on empty map")
+
+        try:
+            return self[None]
+        except KeyError:
+            return self[min(self)]  # type: ignore
---
+++ test/unit/test_language.py
@@ -0,0 +1,579 @@
+from __future__ import annotations
+
+from typing import Any
+
+import unittest
+import collections.abc
+
+from nbxmpp.language import basic_filter_languages
+from nbxmpp.language import LanguageMap
+from nbxmpp.language import LanguageRange
+from nbxmpp.language import LanguageTag
+from nbxmpp.language import lookup_language
+
+
+class TestLanguageTag(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(TypeError):
+            LanguageTag("foo")
+
+        with self.assertRaises(ValueError):
+            LanguageTag(tag="")
+
+        with self.assertRaises(TypeError):
+            LanguageTag()
+
+    def test_fromstr_match_str(self):
+        tag = LanguageTag.fromstr("de-Latn-DE-1999")
+        self.assertEqual("de-latn-de-1999", tag.match_str)
+
+    def test_fromstr_print_str(self):
+        tag = LanguageTag.fromstr("de-Latn-DE-1999")
+        self.assertEqual("de-Latn-DE-1999", tag.print_str)
+
+    def test___str__(self):
+        tag = LanguageTag.fromstr("zh-Hans")
+        self.assertEqual("zh-Hans", str(tag))
+        tag = LanguageTag.fromstr("de-Latn-DE-1999")
+        self.assertEqual("de-Latn-DE-1999", str(tag))
+
+    def test_compare_case_insensitively(self):
+        tag1 = LanguageTag.fromstr("de-DE")
+        tag2 = LanguageTag.fromstr("de-de")
+        tag3 = LanguageTag.fromstr("fr")
+
+        self.assertTrue(tag1 == tag2)
+        self.assertFalse(tag1 != tag2)
+        self.assertTrue(tag2 == tag1)
+        self.assertFalse(tag2 != tag1)
+
+        self.assertTrue(tag1 != tag3)
+        self.assertFalse(tag1 == tag3)
+        self.assertTrue(tag2 != tag3)
+        self.assertFalse(tag2 == tag3)
+
+        self.assertTrue(tag3 != tag1)
+        self.assertFalse(tag3 == tag1)
+        self.assertTrue(tag3 != tag1)
+        self.assertFalse(tag3 == tag1)
+
+    def test_order_case_insensitively(self):
+        tag1 = LanguageTag.fromstr("de-DE")
+        tag2 = LanguageTag.fromstr("de-de")
+        tag3 = LanguageTag.fromstr("en-us")
+        tag4 = LanguageTag.fromstr("fr")
+
+        self.assertLess(tag1, tag3)
+        self.assertLess(tag1, tag4)
+        self.assertLess(tag2, tag3)
+        self.assertLess(tag2, tag4)
+        self.assertLess(tag3, tag4)
+
+        self.assertGreater(tag4, tag3)
+        self.assertGreater(tag4, tag2)
+        self.assertGreater(tag4, tag1)
+        self.assertGreater(tag3, tag2)
+        self.assertGreater(tag3, tag1)
+
+        self.assertFalse(tag1 > tag2)
+        self.assertFalse(tag2 > tag1)
+
+        self.assertFalse(tag1 < tag2)
+        self.assertFalse(tag2 < tag1)
+
+    def test_hash_case_insensitively(self):
+        tag1 = LanguageTag.fromstr("de-DE")
+        tag2 = LanguageTag.fromstr("de-de")
+
+        self.assertEqual(hash(tag1), hash(tag2))
+
+    def test_not_equal_to_None(self):
+        tag1 = LanguageTag.fromstr("de-DE")
+        self.assertNotEqual(tag1, None)
+
+    def test_dont_compare_with_None(self):
+        tag1 = LanguageTag.fromstr("de-DE")
+        with self.assertRaises(TypeError):
+            tag1 > None
+        with self.assertRaises(TypeError):
+            tag1 < None
+        with self.assertRaises(TypeError):
+            tag1 >= None
+        with self.assertRaises(TypeError):
+            tag1 <= None
+
+    def test__repr__(self):
+        tag1 = LanguageTag.fromstr("de-DE")
+        tag2 = LanguageTag.fromstr("fr")
+
+        self.assertEqual("<nbxmpp.language.LanguageTag.fromstr('de-DE')>", repr(tag1))
+
+        self.assertEqual("<nbxmpp.language.LanguageTag.fromstr('fr')>", repr(tag2))
+
+    def test_immutable(self):
+        tag = LanguageTag.fromstr("foo")
+        with self.assertRaises(AttributeError):
+            tag.foo = "bar"
+
+
+class TestLanguageRange(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(TypeError):
+            LanguageRange("foo")
+
+        with self.assertRaises(ValueError):
+            LanguageRange(tag="")
+
+        with self.assertRaises(TypeError):
+            LanguageRange()
+
+    def test_fromstr_match_str(self):
+        tag = LanguageRange.fromstr("de-DE")
+        self.assertEqual("de-de", tag.match_str)
+
+    def test_fromstr_print_str(self):
+        tag = LanguageRange.fromstr("de-Latn-DE-1999")
+        self.assertEqual("de-Latn-DE-1999", tag.print_str)
+
+    def test___str__(self):
+        tag = LanguageRange.fromstr("zh-Hans")
+        self.assertEqual("zh-Hans", str(tag))
+        tag = LanguageRange.fromstr("de-Latn-DE-1999")
+        self.assertEqual("de-Latn-DE-1999", str(tag))
+
+    def test_compare_case_insensitively(self):
+        tag1 = LanguageRange.fromstr("de-DE")
+        tag2 = LanguageRange.fromstr("de-de")
+        tag3 = LanguageRange.fromstr("fr")
+
+        self.assertTrue(tag1 == tag2)
+        self.assertFalse(tag1 != tag2)
+        self.assertTrue(tag2 == tag1)
+        self.assertFalse(tag2 != tag1)
+
+        self.assertTrue(tag1 != tag3)
+        self.assertFalse(tag1 == tag3)
+        self.assertTrue(tag2 != tag3)
+        self.assertFalse(tag2 == tag3)
+
+        self.assertTrue(tag3 != tag1)
+        self.assertFalse(tag3 == tag1)
+        self.assertTrue(tag3 != tag1)
+        self.assertFalse(tag3 == tag1)
+
+    def test_hash_case_insensitively(self):
+        tag1 = LanguageRange.fromstr("de-DE")
+        tag2 = LanguageRange.fromstr("de-de")
+
+        self.assertEqual(hash(tag1), hash(tag2))
+
+    def test_not_equal_to_None(self):
+        r1 = LanguageRange.fromstr("de-DE")
+        self.assertNotEqual(r1, None)
+
+    def test_wildcard(self):
+        r1 = LanguageRange.fromstr("*")
+        r2 = LanguageRange.fromstr("*")
+        self.assertIs(r1, r2)
+
+    def test_strip_rightmost(self):
+        r = LanguageRange.fromstr("de-Latn-DE-x-foo")
+        self.assertEqual(LanguageRange.fromstr("de-Latn-DE"), r.strip_rightmost())
+        self.assertEqual(
+            LanguageRange.fromstr("de-Latn"), r.strip_rightmost().strip_rightmost()
+        )
+        self.assertEqual(
+            LanguageRange.fromstr("de"),
+            r.strip_rightmost().strip_rightmost().strip_rightmost(),
+        )
+        with self.assertRaises(ValueError):
+            r.strip_rightmost().strip_rightmost().strip_rightmost().strip_rightmost()
+
+    def test_immutable(self):
+        r = LanguageRange.fromstr("foo")
+        with self.assertRaises(AttributeError):
+            r.foo = "bar"
+
+
+class Testbasic_filter_languages(unittest.TestCase):
+    def setUp(self):
+        self.languages = [
+            LanguageTag.fromstr("de-Latn-DE-1999"),
+            LanguageTag.fromstr("de-DE"),
+            LanguageTag.fromstr("de-Latn"),
+            LanguageTag.fromstr("fr-CH"),
+            LanguageTag.fromstr("it"),
+        ]
+
+    def test_filter(self):
+        self.assertSequenceEqual(
+            [
+                self.languages[0],
+                self.languages[1],
+                self.languages[2],
+            ],
+            list(
+                basic_filter_languages(
+                    self.languages,
+                    list(
+                        map(
+                            LanguageRange.fromstr,
+                            [
+                                "de",
+                            ],
+                        )
+                    ),
+                )
+            ),
+        )
+
+        self.assertSequenceEqual(
+            [
+                self.languages[1],
+            ],
+            list(
+                basic_filter_languages(
+                    self.languages,
+                    list(
+                        map(
+                            LanguageRange.fromstr,
+                            [
+                                "de-DE",
+                            ],
+                        )
+                    ),
+                )
+            ),
+        )
+
+        self.assertSequenceEqual(
+            [
+                self.languages[0],
+                self.languages[2],
+            ],
+            list(
+                basic_filter_languages(
+                    self.languages,
+                    list(
+                        map(
+                            LanguageRange.fromstr,
+                            [
+                                "de-Latn",
+                            ],
+                        )
+                    ),
+                )
+            ),
+        )
+
+    def test_filter_no_dupes_and_ordered(self):
+        self.assertSequenceEqual(
+            [
+                self.languages[0],
+                self.languages[2],
+                self.languages[1],
+            ],
+            list(
+                basic_filter_languages(
+                    self.languages,
+                    list(
+                        map(
+                            LanguageRange.fromstr,
+                            [
+                                "de-Latn",
+                                "de",
+                            ],
+                        )
+                    ),
+                )
+            ),
+        )
+
+    def test_filter_wildcard(self):
+        self.assertSequenceEqual(
+            self.languages,
+            list(
+                basic_filter_languages(
+                    self.languages,
+                    list(
+                        map(
+                            LanguageRange.fromstr,
+                            [
+                                "fr",
+                                "*",
+                            ],
+                        )
+                    ),
+                )
+            ),
+        )
+
+
+class Testlookup_language(unittest.TestCase):
+    def setUp(self):
+        self.languages = [
+            LanguageTag.fromstr("de-Latn-DE-1999"),
+            LanguageTag.fromstr("fr-CH"),
+            LanguageTag.fromstr("it"),
+        ]
+
+    def test_match_direct(self):
+        self.assertEqual(
+            LanguageTag.fromstr("fr-CH"),
+            lookup_language(
+                self.languages,
+                list(map(LanguageRange.fromstr, ["en", "fr-ch", "de-de"])),
+            ),
+        )
+
+        self.assertEqual(
+            LanguageTag.fromstr("it"),
+            lookup_language(
+                self.languages,
+                list(
+                    map(
+                        LanguageRange.fromstr,
+                        [
+                            "it",
+                        ],
+                    )
+                ),
+            ),
+        )
+
+    def test_decay(self):
+        self.assertEqual(
+            LanguageTag.fromstr("de-Latn-DE-1999"),
+            lookup_language(
+                self.languages,
+                list(map(LanguageRange.fromstr, ["de-de", "en-GB", "en"])),
+            ),
+        )
+
+        self.assertEqual(
+            LanguageTag.fromstr("fr-CH"),
+            lookup_language(
+                self.languages,
+                list(
+                    map(
+                        LanguageRange.fromstr,
+                        [
+                            "fr-FR",
+                            "de-DE",
+                            "fr",
+                        ],
+                    )
+                ),
+            ),
+        )
+
+    def test_decay_skips_extension_prefixes_properly(self):
+        self.assertEqual(
+            LanguageTag.fromstr("de-DE"),
+            lookup_language(
+                list(
+                    map(
+                        LanguageTag.fromstr,
+                        [
+                            "de-DE",
+                            "de-x",
+                        ],
+                    )
+                ),
+                list(
+                    map(
+                        LanguageRange.fromstr,
+                        [
+                            "de-x-foobar",
+                        ],
+                    )
+                ),
+            ),
+        )
+
+
+class TestLanguageMap(unittest.TestCase):
+    def test_implements_mapping(self):
+        mapping = LanguageMap()
+        self.assertIsInstance(mapping, collections.abc.MutableMapping)
+
+    def test_mapping_interface(self):
+        key1 = LanguageTag.fromstr("de-DE")
+        key2 = LanguageTag.fromstr("en-US")
+        key3 = LanguageTag.fromstr("en")
+
+        mapping = LanguageMap()
+
+        self.assertFalse(mapping)
+        self.assertEqual(0, len(mapping))
+
+        mapping[key1] = 10
+
+        self.assertIn(key1, mapping)
+        self.assertEqual(10, mapping[key1])
+
+        self.assertSetEqual({key1}, set(mapping))
+
+        mapping[key2] = 20
+
+        self.assertIn(key2, mapping)
+        self.assertEqual(20, mapping[key2])
+
+        self.assertSetEqual({key1, key2}, set(mapping))
+
+        key2_prime = LanguageTag.fromstr("en-us")
+
+        self.assertIn(key2_prime, mapping)
+        self.assertEqual(20, mapping[key2_prime])
+
+        self.assertNotIn(key3, mapping)
+
+        del mapping[key1]
+
+        self.assertNotIn(key1, mapping)
+
+        mapping.clear()
+
+        self.assertNotIn(key2, mapping)
+
+    def test_lookup(self):
+        key1 = LanguageTag.fromstr("de-DE")
+        key2 = LanguageTag.fromstr("en-US")
+        key3 = LanguageTag.fromstr("en")
+
+        mapping = LanguageMap()
+
+        mapping[key1] = 10
+        mapping[key2] = 20
+        mapping[key3] = 30
+
+        self.assertEqual(30, mapping.lookup([LanguageRange.fromstr("en-GB")]))
+
+    def test_values(self):
+        key1 = LanguageTag.fromstr("de-DE")
+        key2 = LanguageTag.fromstr("en-US")
+        key3 = LanguageTag.fromstr("en")
+
+        mapping = LanguageMap()
+
+        mapping[key1] = 10
+        mapping[key2] = 20
+        mapping[key3] = 30
+
+        self.assertSetEqual({10, 20, 30}, set(mapping.values()))
+
+    def test_keys(self):
+        key1 = LanguageTag.fromstr("de-DE")
+        key2 = LanguageTag.fromstr("en-US")
+        key3 = LanguageTag.fromstr("en")
+
+        mapping = LanguageMap()
+
+        mapping[key1] = 10
+        mapping[key2] = 20
+        mapping[key3] = 30
+
+        self.assertSetEqual({key1, key2, key3}, set(mapping.keys()))
+
+    def test_items(self):
+        key1 = LanguageTag.fromstr("de-DE")
+        key2 = LanguageTag.fromstr("en-US")
+        key3 = LanguageTag.fromstr("en")
+
+        mapping = LanguageMap()
+
+        mapping[key1] = 10
+        mapping[key2] = 20
+        mapping[key3] = 30
+
+        self.assertSetEqual(
+            {
+                (key1, 10),
+                (key2, 20),
+                (key3, 30),
+            },
+            set(mapping.items()),
+        )
+
+    def test_equality(self):
+        mapping1 = LanguageMap()
+        mapping1[LanguageTag.fromstr("de-de")] = 10
+        mapping1[LanguageTag.fromstr("en-US")] = 20
+
+        mapping2 = LanguageMap()
+        mapping2[LanguageTag.fromstr("de-DE")] = 10
+        mapping2[LanguageTag.fromstr("en-US")] = 20
+
+        mapping3 = LanguageMap()
+        mapping3[LanguageTag.fromstr("de-DE")] = 10
+        mapping3[LanguageTag.fromstr("en-GB")] = 20
+
+        self.assertEqual(mapping1, mapping2)
+        self.assertFalse(mapping1 != mapping2)
+
+        self.assertNotEqual(mapping1, mapping3)
+        self.assertFalse(mapping1 == mapping3)
+
+        self.assertNotEqual(mapping2, mapping3)
+        self.assertFalse(mapping2 == mapping3)
+
+    def test_setdefault(self):
+        l: list[Any] = []
+        mapping = LanguageMap()
+        result = mapping.setdefault(LanguageTag.fromstr("de-de"), l)
+
+        self.assertIs(result, l)
+
+        result = mapping.setdefault(LanguageTag.fromstr("de-de"), [])
+
+        self.assertIs(result, l)
+
+    def test_lookup_returns_None_key_if_nothing_matches(self):
+        mapping = LanguageMap()
+        mapping[None] = "foobar"
+        mapping[LanguageTag.fromstr("de")] = "Test"
+        mapping[LanguageTag.fromstr("en")] = "test"
+
+        self.assertEqual("foobar", mapping.lookup([LanguageRange.fromstr("it")]))
+
+    def test_any_returns_only_key(self):
+        m = LanguageMap()
+        m[None] = "fnord"
+
+        self.assertEqual(m.any(), "fnord")
+
+        m = LanguageMap()
+        m[LanguageTag.fromstr("de")] = "Test"
+
+        self.assertEqual(m.any(), "Test")
+
+    def test_any_raises_ValueError_on_empty_map(self):
+        m = LanguageMap()
+        with self.assertRaises(ValueError):
+            m.any()
+
+    def test_any_prefers_None(self):
+        m = LanguageMap()
+        m[LanguageTag.fromstr("de")] = "Test"
+        m[None] = "fnord"
+
+        self.assertEqual(m.any(), "fnord")
+
+        m = LanguageMap()
+        m[None] = "fnord"
+        m[LanguageTag.fromstr("de")] = "Test"
+
+        self.assertEqual(m.any(), "fnord")
+
+    def test_any_returns_same_key_for_same_keyset(self):
+        m = LanguageMap()
+        m[LanguageTag.fromstr("de")] = "Test"
+        m[LanguageTag.fromstr("fr")] = "fnord"
+
+        self.assertEqual(m.any(), "Test")
+
+        m = LanguageMap()
+        m[LanguageTag.fromstr("fr")] = "fnord"
+        m[LanguageTag.fromstr("de")] = "Test"
+
+        self.assertEqual(m.any(), "Test")
