0f9fcd4097600f068be0e5691a8a11c280bc62de
wurstsalat <mailtrash@posteo.de>
2025-02-15 12:28:48 +0100

    refactor: Remove idlequeue module

--- test/lib/xmpp_mocks.py
+++ test/lib/xmpp_mocks.py
@@ -2,64 +2,10 @@
 Module with dummy classes for unit testing of XMPP and related code.
 """
 
-import threading
-import time
 from unittest.mock import Mock
 
-from nbxmpp import idlequeue
 
-IDLEQUEUE_INTERVAL = 0.2  # polling interval. 200ms is used in Gajim as default
-IDLEMOCK_TIMEOUT = 30  # how long we wait for an event
-
-
-class IdleQueueThread(threading.Thread):
-    """
-    Thread for regular processing of idlequeue.
-    """
-
-    def __init__(self):
-        self.iq = idlequeue.SelectIdleQueue()
-        self.stop = threading.Event()  # Event to stop the thread main loop.
-        self.stop.clear()
-        threading.Thread.__init__(self)
-
-    def run(self):
-        while not self.stop.isSet():
-            self.iq.process()
-            time.sleep(IDLEQUEUE_INTERVAL)
-
-    def stop_thread(self):
-        self.stop.set()
-
-
-class IdleMock:
-    """
-    Serves as template for testing objects that are normally controlled by GUI.
-    Allows to wait for asynchronous callbacks with wait() method.
-    """
-
-    def __init__(self):
-        self._event = threading.Event()
-        self._event.clear()
-
-    def wait(self):
-        """
-        Block until some callback sets the event and clearing the event
-        subsequently.
-        Returns True if event was set, False on timeout
-        """
-        self._event.wait(IDLEMOCK_TIMEOUT)
-        if self._event.isSet():
-            self._event.clear()
-            return True
-        else:
-            return False
-
-    def set_event(self):
-        self._event.set()
-
-
-class MockConnection(IdleMock, Mock):
+class MockConnection(Mock):
     """
     Class simulating Connection class from src/common/connection.py
 
@@ -70,7 +16,6 @@ class MockConnection(IdleMock, Mock):
 
     def __init__(self, *args):
         self.connect_succeeded = True
-        IdleMock.__init__(self)
         Mock.__init__(self, *args)
 
     def on_connect(self, success, *args):
@@ -79,7 +24,6 @@ class MockConnection(IdleMock, Mock):
         from server (NOT after TLS stream restart) or connect failure
         """
         self.connect_succeeded = success
-        self.set_event()
 
     def on_auth(self, con, auth):
         """
@@ -94,4 +38,3 @@ class MockConnection(IdleMock, Mock):
         """
         self.auth_connection = con
         self.auth = auth
-        self.set_event()
