a6f76a9df2edaac55a48504985b4103da3bef526
Philipp Hörist <philipp@hoerist.com>
2026-08-23 23:52:21 +0200

    cfix: Don't validate base64 payloads

--- nbxmpp/util.py
+++ nbxmpp/util.py
@@ -61,7 +61,9 @@ def b64decode(data: str | bytes) -> bytes:
     if isinstance(data, str):
         data = data.encode()
 
-    return base64.b64decode(data, validate=True)
+    # Don't validate base64 data because in XMPP whitespace in
+    # base64 payloads must be accepted
+    return base64.b64decode(data)
 
 
 def b64encode(data: str | bytes) -> str:
